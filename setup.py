abae6b1ed3f782bc222e4ec0463d83d4ffff31ff
Walter Martins-Filho <waltersmartinsf@gmail.com>
2016-05-31 14:18:09 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='exotred',
+      version='0.1',
+      description='Exoplanetary Task Reduction',
+      url='https://github.com/waltersmartinsf/ExoTRed',
+      author='Walter Silva Martins Filho',
+      author_email='walter@on.br',
+      license=' Creative Commons Attribution 4.0 International License',
+      packages=['exotred'],
+      zip_safe=False)
