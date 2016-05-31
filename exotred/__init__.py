12bd419331a51776c54b935348ee13dfdb916aca
Walter Martins-Filho <waltersmartinsf@gmail.com>
2016-05-31 15:44:40 -0300

    masterflat routine

--- exotred/__init__.py
+++ exotred/__init__.py
@@ -18,7 +18,8 @@ import glob #package for list files
 import os #package for control bash commands
 import yaml #input data without any trouble
 import string
-
+import numpy as np
+from astropy.io import fits
 #******************************************************************************
 #**************** BEGIN INPUT PATH FILE ***************************************
 #******************************************************************************
@@ -121,6 +122,120 @@ def masterbias(data_path, save_path, input_file):
     #END of the masterbias reduction messsage
     print '\nsuperbias.fits created!\n'
     print '\nEND of superbias reduction!\n'
+    #obtain the value of return
+    if output == 1:
+        print '!!! ERROR/WARNING !!!'
+        print 'Check if the superbias was created or if there is more than one superbias image.'
+    return output
+
+def masterflat(data_path,save_path,input_file):
+    """
+    Obtain the masterflat image for calibration.
+    ___
+    INPUT:
+    For obtain this parameters, use the input_info function.
+
+    data_path: string, path where are the images data.
+    save_path: string, path where will save all reduced images.
+    input_file: dict, with information describe in the YAML file.
+
+    OUTPUT:
+    It is possible that the function return some of these values:
+
+    0. Create the masterflat image on the save_path.
+    1. It do not create the masterflat image, because of some erros.
+    """
+    #set original directory
+    original_path = os.getcwd()
+    #Change your directory to data diretory
+    os.chdir(data_path)
+    #list all flat images
+    flat = glob.glob('flat*.fits')
+    print 'Loading flat images \nTotal of flat files = ',len(flat),'\nFiles = \n'
+    print flat
+    #if save_path exist, continue; if not, create.
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    #create a list of bias images and copy images to save_path
+    os.system('cp flat*.fits '+save_path)
+    #creating the names of flat with bias subctracted
+    bflat = []
+    for i in flat:
+        bflat.append('B'+i)
+    print '\n Names os flat images with bias subtracted: \n \n',bflat
+    #change for save_path directory
+    os.chdir(save_path)
+    #verify if previous superbias exist
+    if os.path.isfile('superflat.fits') == True:
+        os.system('rm superflat.fits')
+    #verify if exits previous bflat*.fits files and remove then.
+    for i in bflat:
+        if os.path.isfile(i) == True:
+            os.system('rm -f '+i)
+    print '\nCreating superflat .... \n'
+    #create the list of flat images  and bflat images
+    #flat = string.join(flat,',')
+    #bflat = string.join(bflat,',')
+    print '\n Subtracting bias from flat images and creating bflat images.... \n'
+    #iraf.imarith()
+    for i in range(len(flat)):
+        iraf.imarith(flat[i],'-','superbias.fits',bflat[i])
+        #print statistics from bflat*.fits images
+        iraf.imstat(bflat[i])
+    print '\n .... done \n'
+    #clean previos flat*.fits files
+    print '\n Clean flat*.fits images .... \n'
+    os.system('rm flat*.fits')
+    print '\n .... done. \n'
+    #normalizing each flat
+    print '\nNormalizing each flat ....\n'
+    #checking if mean from numpy is the same from your bflat images using imstat
+    #take the mean of each bflat image
+    bflat_mean = np.zeros(len(bflat))
+    for i in range(len(bflat)):
+        image = fits.getdata(bflat[i])
+        image = np.array(image,dtype='Float64')
+        bflat_mean[i] = round(np.mean(image))
+    image = 0 #clean image allocate to this variable
+    print 'The mean of each bflat image, respectivaly ...'
+    print bflat_mean
+    #creating the names of bflat images after the normalization:
+    abflat = []
+    for i in bflat:
+        abflat.append('A'+i)
+    print '\n Names os bflat images with bias subtracted and normalizad: \n \n',abflat
+    #verify if exist previous ABflat*.fits images and remove then.
+    for i in abflat:
+        if os.path.isfile(i) == True:
+            os.system('rm -f '+i)
+    for i in range(len(abflat)):
+        iraf.imarith(bflat[i],'/',bflat_mean[i],abflat[i])
+    print '\n.... done!\n'
+    print '\n Cleaning bflat*.fits images ....\n'
+    os.system('rm Bflat*.fits')
+    print '\n.... done.\n'
+    print 'Statistics of the abflat*.fits images .... \n'
+    for i in range(len(abflat)):
+        iraf.imstat(abflat[i])
+    print '\n Combining abflat images ....\n'
+    ablist = string.join(abflat,',')
+    iraf.imcombine(ablist,'superflat.fits')
+    iraf.imstat('superflat.fits')
+    print '\n .... done. \n'
+    print '\nCleaning ABflat*.fits images ....\n'
+    os.system('rm ABflat*.fits')
+    print '\n.... done!'
+    #Verify if the image was created:
+    output = glob.glob('superflat*.fits')
+    if len(output) != 0:
+        output = 0
+    else:
+        output = 1
+    #Return to original directory
+    os.chdir(original_path)
+    #last mensage
+    print '\n MASTERFLAT.FITS created! \n'
+    print '\n END of Data Reduction for create a masterflat.fits file. \n'
     #obtain the value of return
     if output == 1:
         print '!!! ERROR/WARNING !!!'
